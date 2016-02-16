b8957496df38a0d6de2c29b52f086f6b867af8c4
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-02-16 10:13:48 +0100

    metaloop works better

--- logic.rb
+++ logic.rb
@@ -3,24 +3,37 @@ require_relative 'rule'
 
 # Check this out
 
+def clone_ruleset(ruleset)
+  new_ruleset = {}
+  ruleset.each do |condition, reaction_array|
+    new_ruleset[condition] = reaction_array.clone
+  end
+  new_ruleset
+end
+
 def logic_this(arg_facts, arg_rules, query)
   original_facts = Facts.new(*arg_facts)
 
   puts 'Original Facts:'
   puts original_facts.to_s
 
+  new_rulesets = []
   rulesets = [arg_rules]
-  rulesets.map! do |ruleset|
+  i = 0
+  while rulesets[i] != nil
+    ruleset = rulesets[i]
     new_ruleset = []
     ruleset.each do |condition, reaction_array|
       raise 'reaction array is empty!' if reaction_array.empty?
-      reaction = reaction_array.shift
-      # if reaction_array is a clone, use below
-      # reaction = ruleset[condition].shift
-      rulesets << ruleset.clone unless reaction_array.empty?
-      new_ruleset << Rule.new(condition, reaction)
+      new_ruleset << Rule.new(condition, reaction_array.first)
+      if reaction_array.count > 1
+        temp_ruleset = clone_ruleset(ruleset)
+        temp_ruleset[condition].shift
+        rulesets << temp_ruleset
+      end
     end
-    new_ruleset
+    new_rulesets << new_ruleset
+    i += 1
   end
 
   puts ''
@@ -28,11 +41,11 @@ def logic_this(arg_facts, arg_rules, query)
   puts '===================='
 
   begin
-    all_facts = rulesets.map do |ruleset|
+    all_facts = new_rulesets.map do |ruleset|
 
       # debug v
       puts 'Rules:'
-      puts ruleset.map(&:to_s).join('\n')
+      puts ruleset.map(&:to_s).join("\n")
       puts ''
       puts 'Facts:'
       # debug ^
@@ -83,8 +96,8 @@ end
 # Examples
 
 arg_facts = [:A, :B]
-arg_rules = {'facts[:A]' => [{C: true}, {D: true}]}
-# arg_rules = {'facts[:A]' => [{X: true}, {Y: true}], 'facts[:X]' => [{C: true}, {D: true}]}
+# arg_rules = {'facts[:A]' => [{C: true}, {D: true}]}
+arg_rules = {'facts[:A]' => [{X: true}, {Y: true}], 'facts[:X]' => [{C: true}, {D: true}]}
 query = [:A, :B, :C, :D]
 
 logic_this(arg_facts, arg_rules, query)
